9f0b0b1262720f4f0fd30ce8d5af1a2a303f5236
Jonathan Bry <jonathanbry@Davids-MacBook-Pro.local>
2022-04-28 20:42:27 -0700

    final

--- src/components/StakedBalance.jsx
+++ src/components/StakedBalance.jsx
@@ -214,7 +214,7 @@ const StakedBalance = () => {
                   chain: "matic",
                   contractAddress: "0xB0b0b94DefD5d3a4898F01A6c773d75710c2537E",
                   functionName: "get",
-                  abi: [{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"get","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"}],
+                  abi: [{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"get","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}],
             params: { user: userAddress.attributes.accounts[0] }
               });
 
